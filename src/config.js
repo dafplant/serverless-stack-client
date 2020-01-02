4956d3aceabf93ed8b73b03472f5bb77fe57af07
Christopher Plant <chris.daf@gmail.com>
2020-01-02 16:26:41 +0000

    Added AWS Amplify into app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "eu-west-1",
+      BUCKET: "daf-notes-uploads"
+    },
+    apiGateway: {
+      REGION: "eu-west-1",
+      URL: "https://sr4u75y3ab.execute-api.eu-west-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "eu-west-1",
+      USER_POOL_ID: "eu-west-1_Plwo6nzQE",
+      APP_CLIENT_ID: "6kttagiu9csbc75r2ur4jqipai",
+      IDENTITY_POOL_ID: "eu-west-1:ef3cb42e-1a9c-4a48-8a3f-3486a96b19a8"
+    }
+  };
